1634474d1dd93be5ba3a8909c2761f0d4e8d2acb
DavidMejia2005 <davidjohanmejiamantilla@gmail.com>
2024-12-13 23:22:38 -0500

    Update productoRoutes.js

--- src/routes/productoRoutes.js
+++ src/routes/productoRoutes.js
@@ -1,10 +1,13 @@
 // src/routes/userRoutes.js
 const express = require('express');
-const { crearProducto, obtenerProductos} = require('../controllers/productoController.js');  // Importa el controlador
+const { crearProducto, obtenerProductos, obtenerProductoPorNombre } = require('../controllers/productoController.js');
+
 const router = express.Router();
 
 // Ruta para crear un usuario
 router.post('/crearproducto', crearProducto);
 router.get('/obtenerproductos', obtenerProductos);
+router.get('/obtenerproducto/:nombre', obtenerProductoPorNombre); // Nueva ruta para filtrar
+
 
 module.exports = router;
